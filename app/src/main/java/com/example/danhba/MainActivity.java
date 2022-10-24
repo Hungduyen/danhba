1ec148d75aa38aa6178a3492e8119356b0ad2eaf
DIEN MAY XANH <nguyentronghung3005@gmail.com>
2022-10-24 19:05:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/danhba/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.danhba;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnGetContact(View v){
+
+        getPhone();
+    }
+
+    private void getPhone(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS}, 0);
+        }
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        Log.i("Danh bạ", "Tổng số liên hệ hiện có là: " + Integer.toString(cursor.getCount()));
+        if(cursor.getCount() > 0){
+            while (cursor.moveToNext()) {
+                String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String contactNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                Log.i("Danh bạ", " Tên liên hệ: " + contactName + " Số điện thoại:" + contactNumber);
+            }
+        }
+    }
+}
